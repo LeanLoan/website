35dfe172ed6f599b5d3a09d57c454f9d526930ab
Alan Hortz <alan.hortz@gmail.com>
2017-03-29 12:44:43 +0200

    refactoring of the javascript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
-$(document).foundation()
+(function(){
+
+	$(document).foundation();
+
+	var heroBtnLearnMore = $('#hero-btn-learn-more');
+	var heroBtnDownloadContract = $('#hero-btn-download-contract');
+
+	heroBtnLearnMore.click(function(event) {
+		console.log('test');
+		// ga('send', 
+		// 	'event',
+		// 	'Hero-LearnMore',
+		// 	'click',
+		// 	'Landing Page Beta', 
+		// 	{
+		// 		nonInteraction: true
+		// 	});
+	});
+
+	heroBtnDownloadContract.click(function(event) {
+		console.log('test');
+		// ga('send',
+		// 	'event',
+		// 	'Contract',
+		// 	'download',
+		// 	'Landing Page Beta',
+		// 	{
+  // 				nonInteraction: true
+		// 	}
+		// );
+	});
+
+})();
+
+
