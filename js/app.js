c97ab6858fbad666f82a089d4e67f29d473fafd1
Alan Hortz <alan.hortz@gmail.com>
2017-04-25 15:40:19 +0200

    last version

--- js/app.js
+++ js/app.js
@@ -3,38 +3,105 @@ $(document).foundation();
 
 $(document).ready(function(){
 
-	var heroBtnLearnMore = $('#hero-btn-learn-more');
+	
 	var heroBtnDownloadContract = $('#hero-btn-download-contract');
+	var modalBtnSubmitEmail = $('#modal-btn-submit-email');
+	var modalEmailContract = $('#modal-email-contract');
+	var modalEmailConfirmation = $('#modal-email-confirmation');
+	var modalConfirmationBtnDownload = $('#modal-confirmation-btn-download');
+	var modalBtnClose = $('#modal-btn-close');
+	var mceEMAIL = $('#mce-EMAIL');
+	var mcEmbeddedSubscribeForm = $('#mc-embedded-subscribe-form');
+	
 
-	heroBtnLearnMore.click(function(event) {
-		ga('send', 
-			'event',
-			'Hero Button Learn More',
-			'click',
-			'Landing Page Beta', 
-			{
-				nonInteraction: true
-			});
-	});
 
 	heroBtnDownloadContract.click(function(event) {
-		ga('send',
+		ga(	'send',
 			'event',
 			'Hero Button Contract',
 			'download',
-			'Landing Page Beta',
-			{
-  				nonInteraction: true
-			}
-		);
+			'NL WEBSITE 23/04',
+			{nonInteraction: true});	
+		});
+
+
+	modalBtnClose.click(function(event) {
+		console.log('modal closed');
+		ga(	'send',
+			'event',
+			'Modal closed',
+			'download',
+			'NL Website 23/04',
+			{nonInteraction: true});		
 	});
 
-	$('.hero-slideshow').slick({
-		autoplay: true,
-		autoplaySpeed: 1000,
-		fade: true,
-		arrows: false
+	mceEMAIL.click(function(event) {
+		ga(	'send',
+			'event',
+			'Email input clicked',
+			'download',
+			'NL Website 23/04',
+			{nonInteraction: true});		
+
 	});
+
+	modalConfirmationBtnDownload.click(function(event) {
+		ga(	'send',
+			'event',
+			'Download pdf contract',
+			'download',
+			'NL Website 23/04',
+			{nonInteraction: true});
+
+
+	});
+
+    $('#mc-embedded-subscribe-form input[type="submit"]').bind('click', function ( event ) {
+        if ( event ) event.preventDefault();
+        signup(mcEmbeddedSubscribeForm);
+    });
+
+
+	function signup($form) {
+	  
+		ga(	'send',
+			'event',
+			'Push on subscribe',
+			'download',
+			'NL Website 23/04',
+			{nonInteraction: true});		
+
+
+		$.ajax({
+		type: $form.attr('method'),
+		url: $form.attr('action'),
+		data: $form.serialize(),
+		cache       : false,
+		dataType    : 'jsonp',
+		jsonp       : 'c',
+		contentType: "application/json; charset=utf-8",
+		error       : function(err) { alert("Could not connect to the registration server. Please try again later."); },
+		success     : function(data) {
+		    if (data.result != "success") {
+		        
+
+		    } else {
+		        
+		        var myFirebaseRef = firebase.database().ref("contacts");
+		        myFirebaseRef.push({
+		          email: $form[0].elements.EMAIL.value,
+		        }, onSignupComplete);
+		       
+		    }
+		}
+	  });
+	}
+
+	function onSignupComplete() {
+		modalEmailContract.foundation('close');
+		modalEmailConfirmation.foundation('open');		
+
+	};
 
 });
 
